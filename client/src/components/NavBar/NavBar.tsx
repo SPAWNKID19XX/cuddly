28f76806cd5f0915e2c42b39532af76f756639f9
SPAWNKID19XX <borisisac@gmail.com>
2025-06-21 16:50:46 +0100

    [UPDATE] aboutus visual

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import {useState, useEffect, useCallback} from 'react';
 import './NavBar.css';
-import logo from "../../assets/img/logo.jpg"
+import logo from "../../assets/img/logo1.jpg"
 import LanguageSelector from "../LanguageSelector/LanguageSelector.tsx";
 import {useTranslation} from "react-i18next";
 import { Link } from 'react-router-dom';
